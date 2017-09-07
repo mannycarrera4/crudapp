85d574757530ce6626a4b263cd8ea19c5533c6e9
Manuel Carrera <manuel.carrera@workday.com>
2017-09-07 08:10:21 -0600

    clean up html, open modal from edit button

--- app/components/list/listComponent.js
+++ app/components/list/listComponent.js
@@ -2,6 +2,8 @@ angular.module('list.listComponent', [])
   .component('list', {
     templateUrl: 'components/list/list.html',
     controller: function ($http, $uibModal, $scope, routeManager) {
+      this.showModal = false
+      // console.warn(routeManager.getRoutes());
       firebase.database().ref('route').on('value', (snapshot) => {
         const newRouteList = []
         const keyList = []
@@ -15,6 +17,23 @@ angular.module('list.listComponent', [])
 
         $scope.$evalAsync()
       })
+
+      this.open = (routeKey) => {
+        $uibModal.open({
+          component: 'editModal',
+          resolve: {
+            modalData: function() {
+
+            }
+          }
+        }).result.then(function(result) {
+          console.info("I was closed, so do what I need to do myContent's controller now.  Result was->");
+          console.info(result);
+        }, function(reason) {
+          console.info("I was dimissed, so do what I need to do myContent's controller now.  Reason was->" + reason);
+        })
+        this.showModal = true
+      }
 
       this.deleteRouteFromList = (routeKey) => {
         routeManager.deleteRoute(routeKey)
